257db44ae337761e7495d5f98f2660154e633c59
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2021-04-08 19:23:51 -0400

    got it running

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -12,6 +12,26 @@ import {
 
 class Nav extends React.Component {
     render() {
-      return <h1>Hello, {this.props.name}</h1>;
+        return(
+            <Router>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
+                <Link className="navbar-brand" to="/">Micro Blog</Link>
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" exact to="/">
+                        Create a Micro Post
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" exact to="/">
+                        About Us
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+            </Router>
+        )
     }
-  }
+}
+
+export default Nav;
