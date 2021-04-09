96fadae8e8e2bff751b47078b6deba3f6b3204ce
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2021-04-09 12:54:01 -0400

    bad navbar imp

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import logo from './tl-logo.png';
+import './NavBar.css';
 import { 
   BrowserRouter as Router, 
   Switch, 
@@ -7,27 +9,33 @@ import {
   NavLink
 } from 'react-router-dom';
 
-//import './App.css';
+
 
 
 class Nav extends React.Component {
     render() {
         return(
             <Router>
-            <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
-                <Link className="navbar-brand" to="/">Micro Blog</Link>
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <NavLink className="nav-link" exact to="/">
-                        Create a Micro Post
-                        </NavLink>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3 fixed-top">
+                <img src={logo} className="resize navbar-brand" />
+
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                    <a class="nav-link">Sign Up</a>
                     </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" exact to="/">
-                        About Us
-                        </NavLink>
+
+                    <li class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle navbar-toggler-icon" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
                     </li>
+                    
                 </ul>
+
             </nav>
             </Router>
         )
--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,3 +1,14 @@
-import React from 'react';
-import Nac from '../components/NavBar';
+import React, { Component } from 'react';
+import Nav from '../components/NavBar';
 import { Redirect } from 'react-router-dom';
+
+class ProfilePage extends React.Component {
+    render() {
+        return (
+            <Nav />
+        );
+    }
+
+}
+
+export default ProfilePage;
