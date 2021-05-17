556f4c1057e6ec24102d7855b995bf33934fdc0e
Shafiullah Rahman <shafiullahrahman@Shafiullahs-MBP.home>
2021-05-17 16:17:01 -0400

    added firebase storage config obj to the .env file

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/storage'
-import firebaseConfig from './firebaseConfig.json';
+require('dotenv').config();
+// import firebaseConfig from './firebaseConfig.json';
+const firebaseConfig = process.env.FIREBASE_CONFIG;
 
 
 firebase.initializeApp(firebaseConfig);
