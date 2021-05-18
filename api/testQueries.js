09eba96f1a044aca24857f8bda8013eb3db9462e
Shafiullah Rahman <shafiullahrahman@Shafiullahs-MBP.home>
2021-05-18 00:02:26 -0400

    commented out some tests

--- api/testQueries.js
+++ api/testQueries.js
@@ -7,29 +7,29 @@ const Op = Sequelize.Op;
   currently, the Location table only has 5 entries:
   Add one more Location of your choice.
 */
-function insertNewLocation() {
-    return Location.create({state: 'Texas', city: 'Houston', zipCode: 32912, coord: {type: 'Point', coordinates: [-30.12, 150.76]}})
-                .then(newLoc => {
-                    console.log(newLoc.state + " " + newLoc.city)
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-}
+// function insertNewLocation() {
+//     return Location.create({state: 'Texas', city: 'Houston', zipCode: 32912, coord: {type: 'Point', coordinates: [-30.12, 150.76]}})
+//                 .then(newLoc => {
+//                     console.log(newLoc.state + " " + newLoc.city)
+//                 })
+//                 .catch(err => {
+//                     console.log(err)
+//                 })
+// }
 
 /*
   currently, there are 5 Posts
   Add one more Post of your choice.
 */
-function insertNewPost() {
-    return Post.create({body:'lectus in quam fringilla rhoncus.', title: 'Awesome Title', likes: 200, dislikes: 32, locationId: 3})
-               .then(newPost => {
-                   console.log(newPost.title)
-               })
-               .catch(err => {
-                   console.log(err)
-               })
-}
+// function insertNewPost() {
+//     return Post.create({body:'lectus in quam fringilla rhoncus.', title: 'Awesome Title', likes: 200, dislikes: 32, locationId: 3})
+//                .then(newPost => {
+//                    console.log(newPost.title)
+//                })
+//                .catch(err => {
+//                    console.log(err)
+//                })
+// }
 
 /*
   Return the state and city of the location with id=2 as an object containg state and city properties 
@@ -41,42 +41,42 @@ function insertNewPost() {
       city : "cityName"
   }
 */
-async function getLocationWithId2() {
-    let cityStateObj = {}
+// async function getLocationWithId2() {
+//     let cityStateObj = {}
 
-    const loc = await Location.findByPk(2)
+//     const loc = await Location.findByPk(2)
             
-    cityStateObj.state = loc.state
-    cityStateObj.city = loc.city
+//     cityStateObj.state = loc.state
+//     cityStateObj.city = loc.city
 
-    return cityStateObj
-}
+//     return cityStateObj
+// }
 
 /*
   Return an array of all the Post titles
 */
-async function getAllPosts() {
-    const posts = await Post.findAll()
+// async function getAllPosts() {
+//     const posts = await Post.findAll()
 
-    let titles = posts.map(p => {
-        return p.title
-    })
+//     let titles = posts.map(p => {
+//         return p.title
+//     })
 
-    return titles
-}
+//     return titles
+// }
 
 /*
   Return an array of all the location coordinates
 */
-function getAllLocations() { 
-}
+// function getAllLocations() { 
+// }
 
 /*
   Delete the new post you added in the second test
 */
-function deletePostYouAdded() {
-    return Post.destroy({where: {title : "Awesome Title"}})  
-}
+// function deletePostYouAdded() {
+//     return Post.destroy({where: {title : "Awesome Title"}})  
+// }
 
 /*
 Associate User with userName = "okeson0" to posts with postId = [1,3,5]
