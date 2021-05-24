03be308af17cae7e8c38e14bc5fc96ab7b56bfb2
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2021-05-23 22:02:17 -0400

    added background to about us

--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -1,8 +1,52 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import "./AboutUsPage.css";
 
 function AboutUsPage(props) {
   return (
-    <div>A little info about us.</div>
+
+    <div>
+      
+      <Helmet>
+        <body className='about-page' />
+        <html className='about-page' />
+      </Helmet>
+
+      <div>A little info about us.</div>
+
+      <div className="container">
+        <div className="row member">
+            <div className="col">
+              <img alt="shafiulah pic" src="" />
+            </div>
+
+            <div className="col">
+              Im ___. I_____
+            </div>
+        </div>
+
+        <div className="row member">
+            <div className="col">
+              Im ___. I_____
+            </div>
+
+            <div className="col">
+              <img alt="elian pic" src="" />
+            </div>
+        </div>
+
+        <div className="row member">
+            <div className="col">
+              <img alt="henry pic" src="" />
+            </div>
+
+            <div className="col">
+              Im ___. I_____
+            </div>
+        </div>
+      </div>
+
+    </div>
   );
 }
 
